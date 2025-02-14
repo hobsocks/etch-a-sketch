597c0fb21fc30e461fd421ef0155137084e341ba
hobsocks <193536879+hobsocks@users.noreply.github.com>
2025-02-14 15:34:10 -0500

    added grid interactability

--- javascript.js
+++ javascript.js
@@ -1,4 +1,15 @@
-let board = document.querySelector('.board')
+createBoard(16);
 
-board.style.gridTemplateColumns = 'repeat(15, 1fr)';
-board.style.gridTemplateRows = 'repeat(15, 1fr)';
+function createBoard(size){
+    let board = document.querySelector('.board')
+
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let numDivs = size * size;
+
+    for(let i = 0; i < numDivs; i++) {
+        let div = document.createElement("div");
+        board.insertAdjacentElement('beforeend', div);
+    }
+}
